01d97ccd13094b3f54986fbac190495950419985
HP <hp@gmail.com>
2022-07-26 14:58:18 +0300

    q3

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-int sum(int a, int b);
-
-int main()
-{
-    return 0;
-}
-
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
-}
+int main(){
+    int x, y;
+    char q;
+    if(scanf("%d%lc", &x, &y) && ((q == '\n') || (q == ' ')))
+    {
+        if(scanf("%d%c", &y, &q) && ((q == '\n')))
+        {
+            printf("%d %d %d %d", x+y, x-y, x*y, x/y);
+        }
+        else{
+            printf("n/a\n");
+        }
+    }
+    else{
+        printf("n/a\n");
+    return 0;}
+    }
